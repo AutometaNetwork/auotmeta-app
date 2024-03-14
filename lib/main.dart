776d50a18b7b4361388a3220ef543f3305eda34f
MickJord <willzhaoshihao@gmail.com>
2024-03-14 16:06:16 +1100

    feat: display location(longitude, latitude) on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Location Info',
+      home: LocationScreen(),
+    );
+  }
+}
+
+class LocationScreen extends StatefulWidget {
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  late Position _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return;
+      }
+
+      if (permission == LocationPermission.denied) {
+        return;
+      }
+    }
+
+    _currentPosition = await Geolocator.getCurrentPosition();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location Info'),
+      ),
+      body: Center(
+        child: _currentPosition == null
+            ? CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Latitude: ${_currentPosition.latitude}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Longitude: ${_currentPosition.longitude}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
       ),
     );
   }

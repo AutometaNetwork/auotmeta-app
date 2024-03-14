9d3016b536392aaef57426af6625fb34f8222642
MickJord <willzhaoshihao@gmail.com>
2024-03-14 16:16:28 +1100

    fix: init _currentPostition with 0 values for required parameters - avoid variable undefined issues

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,19 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-  late Position _currentPosition;
+  // Position is non-nullable, so init it with all parameters=0
+  Position _currentPosition = Position(
+    longitude: 0,
+    latitude: 0,
+    timestamp: DateTime.now(),
+    accuracy: 0,
+    altitude: 0,
+    altitudeAccuracy: 0,
+    heading: 0,
+    headingAccuracy: 0,
+    speed: 0,
+    speedAccuracy: 0,
+  );  
 
   @override
   void initState() {
